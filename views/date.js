981b64c5abfb0325831e2203de04c95624698e0d
d3marco <36773230+d3marco@users.noreply.github.com>
2021-01-26 18:04:14 -0600

    Add files via upload

---
+++ views/date.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+
+
+exports.getDate = function(){
+
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+
+
+
+}
+exports.getDay = function() {
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+
+
+
+}
